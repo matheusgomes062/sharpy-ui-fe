5116243bad8fc29728d9fddc543e60b233952ace
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-01-16 19:31:11 -0300

    refactor: change Footer <img/> to <Image/> from next

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,48 +1,49 @@
 import React, { FunctionComponent } from 'react';
 import Icon from '@mdi/react';
 import { mdiLinkedin, mdiTwitter, mdiGithub } from '@mdi/js';
+import Image from 'next/image';
 
 const routes = [
-    {
-      title: 'Home',
-      path: ''
-    },
-    {
-      title: 'Quem Somos',
-      path: ''
-    },
-    {
-      title: 'Nosso Time',
-      path: ''
-    },
-    {
-      title: 'Trabalhe Conosco',
-      path: ''
-    },
-    {
-      title: 'Soluções',
-      path: ''
-    },
-    {
-      title: 'Login',
-      path: ''
-    },
-    {
-      title: 'Inscrever-se',
-      path: ''
-    },
-    {
-      title: 'Fale Conosco',
-      path: ''
-    },
-    {
-      title: 'Política de Privacidade',
-      path: ''
-    },
-    {
-      title: 'Termos & Condições',
-      path: ''
-    },
+  {
+    title: 'Home',
+    path: ''
+  },
+  {
+    title: 'Quem Somos',
+    path: ''
+  },
+  {
+    title: 'Nosso Time',
+    path: ''
+  },
+  {
+    title: 'Trabalhe Conosco',
+    path: ''
+  },
+  {
+    title: 'Soluções',
+    path: ''
+  },
+  {
+    title: 'Login',
+    path: ''
+  },
+  {
+    title: 'Inscrever-se',
+    path: ''
+  },
+  {
+    title: 'Fale Conosco',
+    path: ''
+  },
+  {
+    title: 'Política de Privacidade',
+    path: ''
+  },
+  {
+    title: 'Termos & Condições',
+    path: ''
+  },
 ]
   
 const Footer: FunctionComponent = () => {
@@ -50,7 +51,7 @@ const Footer: FunctionComponent = () => {
     <footer className="w-screen bg-black" data-cy="footer">
       <div className="py-20 flex justify-center">
         <div className="mx-10">
-          <img src="/sharpy-logo.svg" alt="An SVG of an eye" />
+          <Image width={181} height={59} src="/sharpy-logo.svg" alt="sharpy SVG" />
           <div className="flex mt-5 space-x-20 md:mt-20 sm:max-md:flex-col-reverse sm:max-md:space-x-0">
             <div className="grid grid-cols-1 gap-3 md:grid-cols-3">
               {routes.slice(0,4).map(route => (<div key={route.title}><a className="text-white">{route.title}</a></div>))}
