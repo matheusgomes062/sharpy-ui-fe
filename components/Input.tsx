b508ad05acacdaa3b2a76ce642d8ac44fc628bc3
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-01-19 17:11:58 -0300

    refactor: remove unnecessary icon colors and add icons props to input

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,4 +1,6 @@
 import React, { FunctionComponent, useState } from 'react'
+import { mdiMagnify, mdiTrayArrowUp } from "@mdi/js";
+import Icon from "@mdi/react";
 
 interface InputProps {
   /**
@@ -8,34 +10,68 @@ interface InputProps {
   /**
    * Input type
    */
-  type: string;
+  type: 'text' | 'file';
+  /**
+   * Input type
+   */
+  icon: 'magnify' | 'trayArrowUp' | 'none';
   /**
    * Click handler
    */
   onChange?: () => void;
 }
 
-/**
- * Input component for user interaction
- */
-const Input: FunctionComponent<InputProps> = ({ placeholder, type, ...props }) => {
+const Icons = {
+  'magnify': mdiMagnify,
+  'trayArrowUp': mdiTrayArrowUp,
+  'none': ''
+};
+
+function show(text: string) { 
+  return <p className="fixed w-64 truncate opacity-40">{text}</p>;
+}
+
+const Input: FunctionComponent<InputProps> = ({ placeholder, type, icon, ...props }) => {
   const [value, setValue] = useState('');
 
-  const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-    setValue(event.target.value);
-  };
+  const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => setValue(event.target.value);
+
+  const shouldOpacityBeZero = () => type === "file" && "opacity-0 max-h-6";
+
+  const isTypeOfFile = () => type === "file" && true;
+
+  const isFileUploaded = () => value;
 
   return (
-    <input
-      type={type}
-      role="input"
-      data-cy="input"
-      placeholder={placeholder}
-      onChange={handleChange}
-      value={value}
-      className="border-solid border-2 border-primary-purple p-2 focus:border-primary-orange focus:outline-primary-orange focus:outline"
-    />
-  )
+    <div
+      className="relative flex items-center justify-between h-12 p-2 border-2 border-solid group w-80 max-h-12 border-primary-purple hover:border-primary-orange hover:outline-primary-orange hover:outline hover:outline-1 sm:max-md:w-full"
+      {...props}
+    >
+      <>
+        {(() => {
+          return (
+            isTypeOfFile() &&
+            (isFileUploaded() ? show(value) : show(placeholder))
+          );
+        })()}
+        <input
+          type={type}
+          role="input"
+          data-cy="input"
+          placeholder={placeholder}
+          onChange={handleChange}
+          value={value}
+          className={`w-full appearance-none outline-0 ${shouldOpacityBeZero()}`}
+        />
+        <Icon
+          path={Icons[icon]}
+          size={1}
+          color="disabled"
+          className="fill-primary-orange group-hover:fill-primary-purple"
+        />
+      </>
+    </div>
+  );
 }
 
 export default Input;
