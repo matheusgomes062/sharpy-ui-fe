3998403eea2c1595d03a71767c4645fa9c22ab57
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-01-21 20:39:46 -0300

    feat: Create SolutionsCard component and SolutionsCardProps

---
+++ types/SolutionsCardProps.ts
@@ -0,0 +1,14 @@
+export default interface ISolutionsCardProps {
+  /**
+  * Icon card
+  */
+  icon: "Devices" | "Integration" | "Screenshot";
+  /**
+  * Title of solution card
+  */
+  title: string;
+  /**
+  * Short description of solution card
+  */
+  shortDescription: string;
+}
