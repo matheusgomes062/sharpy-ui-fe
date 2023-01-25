63e1eeb189d5b26466321b29d4ff950a130255f1
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-01-25 20:52:35 -0300

    feat: create genericTitle component

---
+++ stories/GenericTitle.stories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import GenericTitle from "../components/GenericTitle";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "General/GenericTitle",
+  component: GenericTitle,
+} as ComponentMeta<typeof GenericTitle>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof GenericTitle> = (args) => <GenericTitle {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  title: "Lorem Ipsum",
+  fontStyle: "text-lg font-bold",
+};
