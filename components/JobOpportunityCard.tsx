b520a35a43a112f9aa51184804f17eaa682f5701
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-01-25 20:43:06 -0300

    fix: fix the size of jobOpportunityCard component

--- components/JobOpportunityCard.tsx
+++ components/JobOpportunityCard.tsx
@@ -1,23 +1,9 @@
-import { FunctionComponent } from 'react';
-import Icon from '@mdi/react';
-import { mdiMapMarkerOutline, mdiArrowRight  } from '@mdi/js';
+import { FunctionComponent } from "react";
+import Icon from "@mdi/react";
+import { mdiMapMarkerOutline, mdiArrowRight } from "@mdi/js";
+import IJobOpportunityCardProps from "types/JobOpportunityCardProps";
 
-interface JobOpportunityCardProps {
-  /**
-   * Title of the vacancy
-   */
-  title: string;
-  /**
-   * Country of job
-   */
-  country: string;
-  /**
-   * Modality of job
-   */
-  modality: "Remoto" | "Presencial" | "Híbrido";
-}
-
-const JobOpportunityCard: FunctionComponent<JobOpportunityCardProps> = (
+const JobOpportunityCard: FunctionComponent<IJobOpportunityCardProps> = (
   vacancyCardProps
 ) => {
   const goToVacancyPage = () => {
@@ -26,7 +12,7 @@ const JobOpportunityCard: FunctionComponent<JobOpportunityCardProps> = (
 
   return (
     <div
-      className="flex p-4 mt-4 mr-4 border-2 border-solid cursor-pointer sm:max-md:w-full sm:max-md:mr-0 group h-36 w-80 border-primary-purple hover:border-primary-orange hover:outline-primary-orange hover:outline hover:outline-1"
+      className="flex h-auto p-4 mt-4 mr-4 border-2 border-solid cursor-pointer max-h-44 sm:max-md:w-full sm:max-md:mr-0 group w-80 border-primary-purple hover:border-primary-orange hover:outline-primary-orange hover:outline hover:outline-1"
       onClick={goToVacancyPage}
       data-cy="jobOpportunityCard"
     >
@@ -42,7 +28,9 @@ const JobOpportunityCard: FunctionComponent<JobOpportunityCardProps> = (
             {vacancyCardProps.modality}, {vacancyCardProps.country}
           </p>
         </div>
-        <p className="text-lg font-bold">{vacancyCardProps.title}</p>
+        <p className="text-lg font-bold md:text-base text-mobh3">
+          {vacancyCardProps.title}
+        </p>
       </div>
       <div className="grid place-items-end">
         <Icon
