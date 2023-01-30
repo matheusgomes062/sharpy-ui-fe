4ad9795c07ba3f5ebfe9ed0eca0f13ce25164958
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-01-30 15:13:44 -0300

    refactor: add more props to careers stories

--- stories/Carrers.stories.tsx
+++ stories/Carrers.stories.tsx
@@ -15,8 +15,10 @@ const Template: ComponentStory<typeof Carrers> = (args) => <Carrers {...args} />
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  size: "sm",
-  label: "Lorem Ipsum",
-  description: "Lorem Ipsum",
+  sectionTitle: "Carreiras",
+  description: "Somos uma Software House com propósitos que vão além da tecnologia. Por isso, nosso time é multidisciplinar.",
   mode: "dark",
+  primary: true,
+  size: "base",
+  label: "Ver Mais",
 };
