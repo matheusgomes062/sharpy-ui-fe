0924693a89ac91d4cddaced59e74faf038bc77a8
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-02-01 22:23:42 -0300

    refactor: change the name of property title to genericTitle in IGenericTitle

--- components/GenericTitle.tsx
+++ components/GenericTitle.tsx
@@ -4,7 +4,7 @@ import IGenericTitle from "types/GenericTitle";
 const GenericTitle: FunctionComponent<IGenericTitle> = ({ ...props }) => {
   return (
     <div data-cy="generic-title">
-      <h1 className={`${props.fontStyle}`}>{props.title}</h1>
+      <h1 className={`${props.fontStyle}`}>{props.genericTitle}</h1>
       <div className="h-3 mt-4 w-14 bg-primary-orange" />
     </div>
   );
