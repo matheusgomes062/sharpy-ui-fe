190f9dbd1df2b65fdd63e69fcff505c332f67abd
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-02-01 22:24:23 -0300

    feat: create AboutUs component, props and storybook

---
+++ components/AboutUs.tsx
@@ -0,0 +1,36 @@
+import { FunctionComponent } from "react";
+import SectionTitle from "./SectionTitle";
+import IAboutUs from "types/AboutUsProps";
+import Button from "./Button";
+import GenericTitle from "./GenericTitle";
+
+const AboutUs: FunctionComponent<IAboutUs> = ({ ...props }) => {
+  return (
+    <div
+      className="max-w-6xl p-8 m-auto md:max-xl:w-9/12 md:max-lg:w-11/12"
+      {...props}
+    >
+      <div className="">
+        <SectionTitle title="Quem Somos" description="" mode="dark" />
+        <div className="grid grid-cols-1 gap-x-11 md:grid-cols-2">
+          <div>
+            <img src="/AboutUs.png" />
+          </div>
+          <div className="flex-row place-self-center">
+            <p className="mt-8 lg:mt-0 mb-7">{props.shortDescription}</p>
+            <div className="mb-12 lg:mb-20">
+              <Button
+                label={props.label}
+                size={props.size}
+                primary={props.primary}
+              />
+            </div>
+            <GenericTitle genericTitle={props.genericTitle} fontStyle="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
