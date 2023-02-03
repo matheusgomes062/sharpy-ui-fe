99eecadf63e6a98ea6c972643ff024fd1f7ca6a9
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-02-02 21:16:28 -0300

    feat: add arrow redirect to genericTitle

--- components/GenericTitle.tsx
+++ components/GenericTitle.tsx
@@ -1,11 +1,21 @@
 import { FunctionComponent } from "react";
+import Icon from "@mdi/react";
+import { mdiArrowRight } from "@mdi/js";
 import IGenericTitle from "types/GenericTitle";
 
 const GenericTitle: FunctionComponent<IGenericTitle> = ({ ...props }) => {
+  const redirect = () => {
+    // TODO: redirect to about page
+    return 
+  }
+  
   return (
     <div data-cy="generic-title">
-      <h1 className={`${props.fontStyle}`}>{props.genericTitle}</h1>
-      <div className="h-3 mt-4 w-14 bg-primary-orange" />
+      <div className="flex cursor-pointer group" onClick={redirect}>
+        <h1 className={`${props.fontStyle} mr-3`}>{props.genericTitle}</h1>
+        <Icon path={mdiArrowRight} size={1} color="disabled" className="fill-primary-purple group-hover:fill-primary-orange"/>
+      </div>
+      <div className="h-[7px] mt-4 w-14 bg-primary-orange" />
     </div>
   );
 };
