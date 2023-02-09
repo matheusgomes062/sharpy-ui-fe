83b2c565ee1c113e895387502b161d8a5b29c208
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-02-09 10:13:45 -0300

    refactor: add margin bot property to sectionTitle in components that need it

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -10,17 +10,37 @@ import IContactUsProps from 'types/ContactUsProps';
 const ContactUs: FunctionComponent<IContactUsProps> = ({ title, description, email, phone, address }) => {
 
   return (
-    <div className='w-full flex justify-center h-[480px] bg-primary-gray bg-opacity-30'>
-      <div className='flex flex-col justify-center'>
-        <SectionTitle title={title} description={description} mode="dark" />
-        <div className='flex flex-col md:flex-row md:[&>*]:mr-14 sm:[&>*]:mb-6'>
-          <SmartLink label={email} type={'email'} icon={mdiEmailOutline} iconSize={1} href={'/'}/>
-          <SmartLink label={phone} type={'tel'} icon={mdiPhone} iconSize={1} href={'/'}/>
-          <SmartLink label={address} type={'text'} icon={mdiMapMarkerOutline} iconSize={1} href={'/'}/>
+    <div className="w-full flex justify-center h-[480px] bg-primary-gray bg-opacity-30">
+      <div className="flex flex-col justify-center">
+        <div className="mb-14">
+          <SectionTitle title={title} description={description} mode="dark" />
+        </div>
+        <div className="flex flex-col md:flex-row md:[&>*]:mr-14 sm:[&>*]:mb-6">
+          <SmartLink
+            label={email}
+            type={"email"}
+            icon={mdiEmailOutline}
+            iconSize={1}
+            href={"/"}
+          />
+          <SmartLink
+            label={phone}
+            type={"tel"}
+            icon={mdiPhone}
+            iconSize={1}
+            href={"/"}
+          />
+          <SmartLink
+            label={address}
+            type={"text"}
+            icon={mdiMapMarkerOutline}
+            iconSize={1}
+            href={"/"}
+          />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default ContactUs;
