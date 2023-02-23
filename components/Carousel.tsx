7ec36fd758897be4adb6ab6bb5ac82718585a041
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-02-22 23:35:53 -0300

    feat: add onTouch events to grab events on mobile and add a timer to change the state of carousel

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent, useState, useEffect } from "react";
 import Button from "./Button";
 import CarouselPagination from "./CarouselPagination";
 import ICarousel from "types/CarouselProps";
@@ -14,8 +14,38 @@ const Carousel: FunctionComponent<ICarousel> = (props) => {
 
   const handleSelectedPage = (index: number) => setSelectedPage(index);
 
+  const [startTouchX, setStartTouchX] = useState(0);
+
+  const handleTouchStart = (event: React.TouchEvent) => setStartTouchX(event.touches[0].clientX);
+
+  const handleTouchEnd = (event: React.TouchEvent) => {
+    const endTouchX = event.changedTouches[0].clientX;
+    const touchDiff = endTouchX - startTouchX;
+
+    // Define a threshold (in pixels) to trigger the change of selected statistic block
+    const threshold = 50;
+
+    const moveToRight = touchDiff > threshold && selectedPage > 0;
+    const moveToLeft = touchDiff < -threshold && selectedPage < carouselContent.length - 1;
+
+    moveToRight && handleSelectedPage(selectedPage - 1);
+    moveToLeft && handleSelectedPage(selectedPage + 1);
+  };
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSelectedPage((prevSelectedPage) => prevSelectedPage === carouselContent.length - 1 ? 0 : prevSelectedPage + 1);
+    }, 4000);
+    return () => clearInterval(interval);
+  }, []);
+  
   return (
-    <div className="m-auto 4k:max-w-6xl" data-cy="carousel">
+    <div
+      className="m-auto 4k:max-w-6xl"
+      data-cy="carousel"
+      onTouchStart={handleTouchStart}
+      onTouchEnd={handleTouchEnd}
+    >
       <div className="grid grid-cols-1 md:grid-cols-2">
         <div className="bg-black">
           <div className="flex flex-col max-w-[400px] h-[400px] p-7 m-auto justify-between">
