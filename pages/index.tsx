2e801e64e16e3a5b2db619b6fc7e8389018c69d9
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-02-28 19:38:45 -0300

    feat: create Welcome page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
-import Footer from '../components/Footer';
+import Navbar from "@/components/Navbar";
+import Footer from "../components/Footer";
+import Welcome from "./Welcome";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-between w-screen h-screen overflow-x-hidden">
+      <Navbar />
       <main>
-        <div>Hello World!</div>
+        <Welcome />
       </main>
       <Footer />
     </div>
