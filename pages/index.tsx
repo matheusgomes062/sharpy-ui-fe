f6ca6a2ac86a7e75e8468262e7b559b18adfd6cf
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-03-01 14:51:35 -0300

    refactor: change index.tsx, now only import Welcome page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,7 @@
-import Navbar from "@/components/Navbar";
-import Footer from "../components/Footer";
-import Welcome from "./Welcome";
+import Welcome from "./welcome";
 
-export default function Home() {
+export default function Home() {  
   return (
-    <div className="flex flex-col justify-between w-screen h-screen overflow-x-hidden">
-      <Navbar />
-      <main>
-        <Welcome />
-      </main>
-      <Footer />
-    </div>
+    <Welcome />
   );
 }
