dedc1b5a82dce35202925fc3793f8731198f3bd3
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-03-01 22:08:45 -0300

    fix: fixed the component ContactUs, now it looks like more with the design and has the correct width

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -10,13 +10,15 @@ import IContactUsProps from 'types/ContactUsProps';
 const ContactUs: FunctionComponent<IContactUsProps> = ({ title, description, email, phone, address }) => {
 
   return (
-    <div className='w-full flex justify-center h-[480px] bg-primary-gray bg-opacity-30'>
-      <div className='flex flex-col justify-center'>
-        <SectionTitle sectionTitle={title} description={description} mode="dark" />
-        <div className='flex flex-col md:flex-row md:[&>*]:mr-14 sm:[&>*]:mb-6'>
-          <SmartLink label={email} type={'email'} icon={mdiEmailOutline} iconSize={1} href={'/'}/>
-          <SmartLink label={phone} type={'tel'} icon={mdiPhone} iconSize={1} href={'/'}/>
-          <SmartLink label={address} type={'text'} icon={mdiMapMarkerOutline} iconSize={1} href={'/'}/>
+    <div className='w-full flex justify-center h-[480px] bg-primary-gray bg-opacity-30 p-4'>
+      <div className='flex flex-col justify-center w-full max-w-6xl'>
+        <div className="mb-20">
+          <SectionTitle sectionTitle={title} description={description} mode="dark" />
+        </div>
+        <div className='flex flex-col md:flex-row md:[&>*]:mr-14 sm:[&>*]:mb-6 md:justify-between'>
+          <SmartLink label={email} type={'email'} icon={mdiEmailOutline} iconSize={2} href={'/'}/>
+          <SmartLink label={phone} type={'tel'} icon={mdiPhone} iconSize={2} href={'/'}/>
+          <SmartLink label={address} type={'text'} icon={mdiMapMarkerOutline} iconSize={2} href={'/'}/>
         </div>
       </div>
     </div>
