15b42d453a263502e9a0dcf84819ce419205773b
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-03-05 15:40:40 -0300

    feat: use ourTeam component in aboutUs page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,61 @@
 import Layout from "@/components/Layout";
+import SectionTitle from "@/components/SectionTitle";
+import OurTeam from "@/components/OurTeam";
 
 export default function AboutUs() {
   return (
     <Layout>
-      <h1>Hello World!</h1>
+      <div
+        className={`w-screen md:h-[470px] h-56 bg-[url('/CarouselImg3.jpg')] bg-no-repeat bg-center bg-cover md:bg-fixed`}
+      />
+      <div className="flex justify-center md:my-28 my-9">
+        <div className="w-full max-w-6xl p-4">
+          <SectionTitle
+            mode="dark"
+            sectionTitle="Quem Somos"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Euismod in at eros, aliquam quis ornare faucibus. Metus volutpat, semper et sit volutpat iaculis.
+Non, penatibus quis eros cursus sit diam facilisi pellentesque. Vitae pharetra est nulla laoreet. Diam diam pulvinar ornare felis, dignissim praesent interdum. 
+Mattis vel lacus faucibus lorem semper egestas pulvinar magnis facilisis. Diam dolor lacus hendrerit in arcu."
+          />
+        </div>
+      </div>
+      <OurTeam
+        fontStyle="font-bold"
+        genericTitle="Nosso Time"
+        hasIcon={false}
+        ourTeam={[
+          {
+            name: "Siqueira",
+            position: "Backend Developer",
+            src: "bg-[url('/CarouselImg2.jpg')]",
+          },
+          {
+            name: "Rambo",
+            position: "Backend Developer",
+            src: "bg-[url('/CarouselImg2.jpg')]",
+          },
+          {
+            name: "Gomes",
+            position: "Backend Developer",
+            src: "bg-[url('/CarouselImg2.jpg')]",
+          },
+          {
+            name: "João",
+            position: "Front end Developer",
+            src: "bg-[url('/CarouselImg2.jpg')]",
+          },
+          {
+            name: "Pedro",
+            position: "Front end Developer",
+            src: "bg-[url('/CarouselImg2.jpg')]",
+          },
+          {
+            name: "Vitor",
+            position: "Front end Developer",
+            src: "bg-[url('/CarouselImg2.jpg')]",
+          },
+        ]}
+      />
     </Layout>
   );
 }
