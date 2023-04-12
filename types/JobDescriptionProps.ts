4c41118075af5b95d0c404e8f86538ffd207f433
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-04-12 18:16:35 -0300

    refactor: remove unused props of jobDescription props

--- types/JobDescriptionProps.ts
+++ types/JobDescriptionProps.ts
@@ -1,41 +1,22 @@
 import IButtonProps from "./ButtonProps";
+import IJobOpportunityCardProps from "./JobOpportunityCardProps";
 import ISectionTitleProps from "./SectionTitleProps";
 
-export default interface IJobDescription extends ISectionTitleProps, IButtonProps {
+export default interface IJobDescription extends IButtonProps {
   /**
-   * Id of the job
+   * Id of the Job
    */
-  id: number;
+  id: string | string[] | undefined;
   /**
-   * Title of the job
+   * Description for the title
    */
-  title: string;
+  sectionTitleDescription?: string;
   /**
-   * Country of job
+   * Mode
    */
-  country: string;
+  mode: "light" | "dark";
   /**
    * Short description about the company
    */
   companyDescription: string;
-  /**
-   * what is the mode of the job? remote, hybrid or face-to-face?
-   */
-  modality: string;
-  /**
-   * What is the journey of the job? full time, part time, freelance?
-   */
-  journey: string;
-  /**
-   * List of responsibilities of the job
-   */
-  responsibilities: string[];
-  /**
-   * List of qualifications of the job
-   */
-  qualifications: string[];
-  /**
-   * List of benefits of the job
-   */
-  benefits: string[];
 }
