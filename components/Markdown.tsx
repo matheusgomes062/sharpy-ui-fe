f4486ef625e066686102bea23870a43edd583838
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-04-12 19:51:52 -0300

    refactor: rename function name of Markdown component

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -1,7 +1,7 @@
 import { remark } from "remark";
 import html from "remark-html";
 
-export default function MarkdownFiles({ content }: { content: string }) {
+export default function Markdown({ content }: { content: string }) {
   const processedContent = remark().use(html).processSync(content).toString();
   return <div dangerouslySetInnerHTML={{ __html: processedContent }} />;
 }
