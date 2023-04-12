a8ae027ad25c855f56a9438616c49bbf4e0291b1
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-04-12 14:29:47 -0300

    feat: integrated jobDescription with API

---
+++ pages/api/get-role.ts
@@ -0,0 +1,21 @@
+import { PrismaClient, Role } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+async function GetRoleById(req: NextApiRequest, res: NextApiResponse<Role | null>) {
+  try {
+    const { id } = req.query;
+    const result = await prisma.role.findUnique({
+      where: { id: parseInt(id as string) },
+    });
+    res.status(200).json(result);
+    await prisma.$disconnect();
+  } catch (err: any) {
+    console.log(err);
+    res.status(403);
+    await prisma.$disconnect();
+  }
+}
+
+export default GetRoleById;
