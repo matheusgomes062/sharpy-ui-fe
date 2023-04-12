5c9360ab546c2b58e71c988ef8689c3f3f7c27b9
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-04-12 14:13:37 -0300

    feat: integrated Careers Page with API

--- types/CareersProps.ts
+++ types/CareersProps.ts
@@ -3,10 +3,6 @@ import IJobOpportunityCardProps from "./JobOpportunityCardProps";
 
 export default interface ICareersProps {
   /**
-   * List of jobOpportunities
-   */
-  jobOpportunities: IJobOpportunityCardProps[];
-  /**
    * Input placeholder
    */
   inputPlaceholder: string;
