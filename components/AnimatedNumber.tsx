c2f87138757190378b4c7eccf9a456d820909e9d
Matheus Gomes <matheusgomes062@gmail.com>
2023-04-15 21:16:52 -0300

    refactor: animatednumber has new step input

--- components/AnimatedNumber.tsx
+++ components/AnimatedNumber.tsx
@@ -1,30 +1,39 @@
 import { useEffect, useState, FunctionComponent } from "react";
 
-const AnimatedNumber: FunctionComponent<{ value: number }> = ({ value }) => {
+interface AnimatedNumberProps {
+  value: number;
+  step?: number;
+}
+
+const AnimatedNumber: FunctionComponent<AnimatedNumberProps> = ({ value, step = 1 }) => {
   const [displayedValue, setDisplayedValue] = useState(0);
 
   useEffect(() => {
+    if (isNaN(value)) {
+      console.error(`AnimatedNumber received an invalid value: ${value}`);
+      return;
+    }
+
     const interval = setInterval(() => {
       setDisplayedValue((prevValue) => {
         if (prevValue === value) {
           clearInterval(interval);
           return value;
         }
+
         const diff = value - prevValue;
-        const increment = Math.ceil(Math.abs(diff) / 10);
-        let newValue;
-        if (diff > 0) {
-          newValue = prevValue + increment + Math.floor(Math.random() * 10);
-          newValue = Math.min(newValue, value);
-        } else {
-          newValue = prevValue - increment - Math.floor(Math.random() * 10);
-          newValue = Math.max(newValue, value);
-        }
+        const increment = Math.ceil(Math.abs(diff) / 10) * step;
+        const randomIncrement = step * Math.floor(Math.random() * 3);
+        const newValue = diff > 0
+          ? Math.min(prevValue + increment + randomIncrement, value)
+          : Math.max(prevValue - increment - randomIncrement, value);
+
         return newValue;
       });
     }, 100);
+
     return () => clearInterval(interval);
-  }, [value]);
+  }, [value, step]);
 
   return <span data-cy="animatedNumber">{displayedValue}</span>;
 };
